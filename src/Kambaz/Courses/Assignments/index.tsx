c8c71c3220e83e91d8e613bf8e13db331695df81
Joseph Portas <joeportas05@gmail.com>
2025-07-08 16:44:12 -0400

    Assignment page

--- src/Kambaz/Courses/Assignments/index.tsx
+++ src/Kambaz/Courses/Assignments/index.tsx
@@ -15,9 +15,36 @@ export default function Assignments() {
           >
             A1 - ENV + HTML
           </a>{" "}
+          <div>
+            Multiple Modules | Not Available until July 9th at 12:00 AM | Due
+            July 11th at 11:59 PM | 100 Points
+          </div>
         </li>
+
         <li className="wd-assignment-list-item">
-          {/* Complete On Your Own */}
+          <a
+            href="#/Kambaz/Courses/1234/Assignments/124"
+            className="wd-assignment-link"
+          >
+            A2 - CSS + BOOTSTRAP
+          </a>{" "}
+          <div>
+            Multiple Modules | Not Available until July 11th at 12:00 AM | Due
+            July 13th at 11:59 PM | 100 Points
+          </div>
+        </li>
+
+        <li className="wd-assignment-list-item">
+          <a
+            href="#/Kambaz/Courses/1234/Assignments/125"
+            className="wd-assignment-link"
+          >
+            A3 - JAVASCRIPT + REACT
+          </a>{" "}
+          <div>
+            Multiple Modules | Not Available until July 13th at 12:00 AM | Due
+            July 15th at 11:59 PM | 100 Points
+          </div>
         </li>
       </ul>
     </div>
