2a4be04162a67c711c65f9803cb41effec9c5017
Joseph Portas <joeportas05@gmail.com>
2025-07-08 15:51:56 -0400

    Finished status

--- src/Kambaz/Courses/Home/Status.tsx
+++ src/Kambaz/Courses/Home/Status.tsx
@@ -3,8 +3,13 @@ export default function CourseStatus() {
     <div id="wd-course-status">
       <h2>Course Status</h2>
       <button>Unpublish</button> <button>Publish</button>
-      {/* Complete on your own */}
+      <button>Import existing content</button>
       <button>View Course Notifications</button>
+      <button>Choose home page</button>
+      <button>View course stream</button>
+      <button>New annoucement</button>
+      <button>New analytics</button>
+      <button>View course notifications</button>
     </div>
   );
 }
