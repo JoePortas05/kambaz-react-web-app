808a2b7e465413a90c9b9a0988ea763baabb7119
Joseph Portas <joeportas05@gmail.com>
2025-07-08 23:30:22 -0400

    Added name

--- src/Kambaz/Account/Signin.tsx
+++ src/Kambaz/Account/Signin.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 export default function Signin() {
   return (
     <div id="wd-signin-screen">
+      <h2>Joseph Portas's Page</h2>
       <h3>Sign in</h3>
       <input className="wd-username" placeholder="username" /> <br />
       <input
