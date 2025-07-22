45a82f9d00a92a0e6ef969100dc5e991855b8432
Joseph Portas <joeportas05@gmail.com>
2025-07-21 22:41:35 -0400

    3.2

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -5,7 +5,7 @@ import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
 export default function Labs() {
   return (
-    <div>
+    <div className="p-3">
       <h1>Labs</h1>
       <TOC />
       <Routes>
