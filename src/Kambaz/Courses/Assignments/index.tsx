2c0098060822ee4eedd472d3ed52d687e2ff29a8
Joseph Portas <joeportas05@gmail.com>
2025-07-22 13:57:25 -0400

    All but editor

--- src/Kambaz/Courses/Assignments/index.tsx
+++ src/Kambaz/Courses/Assignments/index.tsx
@@ -1,10 +1,16 @@
 import { Row, Col, Form, InputGroup, Button, ListGroup } from "react-bootstrap";
 import { BsCaretDownFill, BsGripVertical, BsSearch } from "react-icons/bs";
-import { Link } from "react-router";
+import { Link, useParams } from "react-router";
 import ModuleControlButtons from "../Modules/ModuleControlButtons";
 import LessonControlButtons from "../Modules/LessonControlButtons";
+import db from "../../Database";
 
 export default function Assignments() {
+  const { cid } = useParams();
+  console.log("CID is:", cid);
+  console.log("Assignments:", db.assignments);
+
+  const assignments = db.assignments;
   return (
     <div id="wd-assignments">
       <Row className="align-items-center mb-3">
@@ -47,68 +53,32 @@ export default function Assignments() {
           </div>
         </ListGroup.Item>
 
-        <ListGroup.Item className="wd-left-border-success p-2">
-          <div className="d-flex justify-content-between">
-            <div>
-              <BsGripVertical />
+        {assignments
+          .filter((assignment) => assignment.course === cid)
+          .map((assignment: any) => (
+            <div key={assignment._id}>
+              <ListGroup.Item className="wd-left-border-success p-2">
+                <div className="d-flex justify-content-between">
+                  <div>
+                    <BsGripVertical />
 
-              <Link
-                to="123"
-                className="wd-assignment-link fw-bold text-decoration-none text-dark"
-              >
-                A1 – ENV + HTML
-              </Link>
-              <br />
-              <small className="text-muted">
-                Multiple Modules | Not available until July 9 12:00 AM | Due
-                July 11 11:59 PM | 100 pts
-              </small>
+                    <Link
+                      to="123"
+                      className="wd-assignment-link fw-bold text-decoration-none text-dark"
+                    >
+                      {assignment.title}
+                    </Link>
+                    <br />
+                    <small className="text-muted">
+                      Multiple Modules | Not available until July 9 12:00 AM |
+                      Due July 11 11:59 PM | 100 pts
+                    </small>
+                  </div>
+                  <LessonControlButtons />
+                </div>
+              </ListGroup.Item>
             </div>
-            <LessonControlButtons />
-          </div>
-        </ListGroup.Item>
-
-        <ListGroup.Item className="wd-left-border-success p-2">
-          <div className="d-flex justify-content-between">
-            <div>
-              <BsGripVertical />
-
-              <Link
-                to="124"
-                className="wd-assignment-link fw-bold text-decoration-none text-dark"
-              >
-                A2 – CSS + BOOTSTRAP
-              </Link>
-              <br />
-              <small className="text-muted">
-                Multiple Modules | Not Available until July 11th at 12:00 AM |
-                Due July 13th at 11:59 PM | 100 Pointss
-              </small>
-            </div>
-            <LessonControlButtons />
-          </div>
-        </ListGroup.Item>
-
-        <ListGroup.Item className="wd-left-border-success p-2">
-          <div className="d-flex justify-content-between">
-            <div>
-              <BsGripVertical />
-
-              <Link
-                to="125"
-                className="wd-assignment-link fw-bold text-decoration-none text-dark"
-              >
-                A3 – JS + REACT
-              </Link>
-              <br />
-              <small className="text-muted">
-                Multiple Modules | Not Available until July 13th at 12:00 AM |
-                Due July 15th at 11:59 PM | 100 Points
-              </small>
-            </div>
-            <LessonControlButtons />
-          </div>
-        </ListGroup.Item>
+          ))}
       </ListGroup>
     </div>
   );
